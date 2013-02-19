2f55524742b14a9ad3c5464ee5a999eeda134b14
Chase Stevens <chase@chasestevens.com>
2013-02-19 11:56:42 +0000

    Additional work on parsing police website

--- report_parsing.py
+++ report_parsing.py
@@ -4,6 +4,16 @@
 #http://www.lbp.police.uk/information/latest_news/news_archives/2013.aspx
 #http://www.lbp.police.uk/information/latest_news/news_archives.aspx
 
+import urllib2
+import time
+
+DEFAULT_HEADERS = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)',\
+                   'Accept-Language': 'en-us',\
+                   'Accept-Encoding': '',\
+                   'Keep-Alive': '300',\
+                   'Connection': 'keep-alive',\
+                   'Cache-Control': 'max-age=0'}
+
 s1 = '''Lothian and Borders Police are appealing for witnesses following a report of indecent exposure in Edinburgh.
 
 The incident happened around 7.50pm on Tuesday 5 February, in the Leith area of the city.
@@ -72,8 +82,78 @@ A Lothian and Borders Police spokesman said: "We are appealing to anyone in the 
 
 Anyone with any information should contact Lothian and Borders Police on 0131 311 3131, or Crimestoppers, where information can be reported anonymously, on 0800 555 111.'''
 
+def url_request(url):
+    txdata = None
+    txheaders = _default_headers
+    request = urllib2.Request(url, txdata, txheaders)
+    data = urllib2.urlopen(request).read()
+    return data
+
+class Incident(object):
+	def __init__(self,url):
+		self.url = url
+		self.raw_html = None
+		self.report = None
+		self.datetime = None
+		self.crimetype = None
+		self.plaintext_location = None
+		self.coords = (None,None)
+	def download_html(self):
+		self.raw_html = url_request(self.url)
+	def populate_data(self):
+		self.report = get_report(self.raw_html)
+		self.plaintext_location = get_report_location(self.report)
+		self.
+
 road_names = ['avenue','street','road','estate','lane']
+incident_exclusion_list = ['charged','arrested','arrest','arrests','appeal','update','?','']
 
 get_street_locations = lambda split_report: filter(lambda (x,y): y.lower() in road_names and y.istitle(),enumerate(split_report))
 
 explode_report = lambda report: filter(lambda x: len(x)>0, x.report.replace('\n',' ').split(' '))
+
+end_tag = lambda start_tag: start_tag[0] + '/' + start_tag[1:]
+
+def get_report(raw_html):
+	start_report_tag = '</h1>'
+	end_report_tag = '</div>'
+	if not start_report_tag in raw_html:
+		return None
+	raw_html = raw_html[raw_html.find(start_report_tag)+len(start_report_tag):]
+	raw_html = raw_html[:raw_html.find(end_report_tag)]
+	raw_html = raw_html.replace('<br />\n','\n')
+	for tag in ['<p>','<span>','<br />']:
+		raw_html = raw_html.replace(tag,' ')
+		raw_html = raw_html.replace(end_tag(tag),' ')
+	return raw_html
+
+def get_report_location(report):
+	report_words = explode_report(report)
+	try:
+		street_incidence_index = get_street_locations(report_words)[0][0]
+	except IndexError:
+		return None
+	while True: #account for cases like "Queensferry Street Lane"
+		nextword = report_words[street_incidence_index + 1]
+		if nextword.istitle() and nextword.lower() in road_names:
+			street_incidence_index+=1
+		else:
+			break
+	start_location_index = street_incidence_index
+	while True: 
+		prevword = report_words[street_incidence_index - 1]
+		if prevword.istitle() or prevword.isdigit():
+			start_location_index-=1
+		else:
+			break
+	return ' '.join(report_words[start_location_index:street_incidence_index+1])
+
+def get_timedate(raw_html):
+	start_report_tag = '"Template_NewsDate1_lblDate" style="float:right">'
+	end_report_tag = "</span>"
+	raw_html = raw_html[raw_html.find(start_report_tag)+len(start_report_tag)]
+	raw_html = raw_html[:raw_html.find(end_report_tag)]
+	#http://docs.python.org/2/library/time.html#time.strptime
+	timedate = time.strptime(raw_html, "%d %B %Y %H:%M") #day (00-31), month (full), year (full), hour(00-23), : minute
+	return timedate #returns a struct_time http://docs.python.org/2/library/time.html#time.struct_time
+
