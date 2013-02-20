81b466aad9c8297bd4816ce45c4217dbfdcc2aed
Ingvaras Merkys <ingvaras@gmail.com>
2013-02-20 15:35:10 +0000

    appengine server stuff development

---
+++ on_server/saferroute.py
@@ -0,0 +1,29 @@
+print 'Content-Type: text/plain'
+import urllib2
+import json
+import re
+
+def testRoute(origin, destination):
+
+   url = "http://maps.googleapis.com/maps/api/directions/json?mode=walking&sensor=false&alternatives=true&origin=" + "+".join(re.split(" +", origin)) + "&destination=" + "+".join(re.split(" +", destination))
+
+   response = urllib2.urlopen(url)
+   data = json.load(response)
+
+   if data['status'] != 'OK':
+      raise Exception("Unexpected status returned: " + data['status'])
+
+   regex = ur"<b>[0-9]?/?([A-Z].+?)</b>+?"
+   for route in data['routes']:
+      streets = []
+      print "------------------\n"
+      for step in route['legs'][0]['steps']:
+#         print step['html_instructions']
+         if len(re.findall(regex, step['html_instructions']))>0:
+            streets.append(re.findall(regex, step['html_instructions'])[0])
+      streets = list(set(streets))
+      for street in streets:
+         print street
+
+print "testRoute('Edinburgh', 'Glasgow')\n"
+testRoute('Edinburgh', 'Glasgow')
