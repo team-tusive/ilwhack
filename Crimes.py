17c9e7738540d672ab8f35773b1ab4565e6b4748
egzius <egzius@gmail.com>
2013-02-21 22:19:04 +0000

    stuff for better stuff

--- Crimes.py
+++ Crimes.py
@@ -4,7 +4,7 @@ import getLocation
 import math
 
 class Crime:
-    def __init__(self,crimetype,title,url,datet,coords,location):
+    def __init__(self,crimetype,title,url,datet,coords,location,streetname):
         self.crimetype = crimetype
         self.title = title
         self.url = url
@@ -12,10 +12,6 @@ class Crime:
         self.coords = coords
         self.location = location
         self.datesec = datet % 86400
-        if ('none'.lower() not in self.coords[0].lower()):
-            self.street = str(getLocation.getName((float(self.coords[0]),float(self.coords[1]))))
-        else:
-            self.street = None
      
     def getDateTime(self):
         return self.datet
@@ -39,9 +35,14 @@ class Crime:
 
 class Crimes:
 
-    def __init__(self,datapath,typepath):
+    def __init__(self,datapath,typepath, optionalCsvStreetNameCoordinate):
         temp_data = open(datapath,'r')
         data = temp_data.read()
+        street_data = csv.reader(open(optionalCsvStreetNameCoordinate))
+        self.streetscoord = list()
+        for row in street_data:
+            for item in row:
+                self.streetscoord.append(item)
         type_data = csv.reader(open(typepath,'r'))
         self.types = list()
         for row in type_data:
@@ -53,15 +54,19 @@ class Crimes:
         
         while (data.find("}")!=(-1)):
             if data.find("\n")!= (-1):
-                tempdata = self.toCrime(data[data.find("{")+1:data.find("\n")])
+                tempdata = self.toCrime(data[data.find("{")+1:data.find("\n")],self.streetscoord)
                 if tempdata!=None:
                     self.result.append(tempdata)
                 data = data[data.find("\n")+1:]
             else:
-                tempdata =self.toCrime(data)
+                tempdata =self.toCrime(data,self.streetscoord)
                 if tempdata != None:
                     self.result.append(tempdata)
                 data = ""
+        
+        changes = csv.writer(open("output.csv",'w'))
+        for item in self.streetscoord:
+            changes.writerow(item)
 
     def findofTypes (self, crimeTypes):
         finalres= list()
@@ -109,7 +114,7 @@ class Crimes:
 
         
 
-    def toCrime (self,data):
+    def toCrime (self,data,streetscoordtmp):
         try:
             crimetype = data[data.find(":")+1:data.find(",")]
             data = data[data.find(",")+1:]
@@ -139,6 +144,20 @@ class Crimes:
             data = data[data.find(":")+1:]
             data = data[data.find("'")+1:]
             location = data[:data.find("'")]
+
+            street = None
+            if ('none'.lower() not in coords[0].lower()):
+                print (coords)
+                for streetname in self.streetscoord:
+                    if (float(coords[0]),float(coords[1])) == streetname[0]:
+                        street = streetname[1]
+                print('works')
+                if street== None:
+                    street = str(getLocation.getName((float(coords[0]),float(coords[1]))))
+                    self.streetscoord.append(((float(coords[0]),float(coords[1])),street))
+
+
+            
             if 'none' in crimetype.lower():
                 for item in self.types:
                     if item.lower() in title.lower():
@@ -146,7 +165,7 @@ class Crimes:
                             break
             if (coords!= ["None","None"]) :
                 if ( (math.sqrt((float(coordx)-55.954198)**2+(float(coordy) + 3.188782)**2)<2)):     
-                    return Crime(crimetype,title,url,datetimesec,coords,location)
+                    return Crime(crimetype,title,url,datetimesec,coords,location,street)
                 return None
         except:
             return None
