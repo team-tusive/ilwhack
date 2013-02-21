3fda4fc75c147732fc77c75f6b8f89a4750d09bf
Jonas Olsson <github.4.jonols@spamgourmet.com>
2013-02-21 11:26:04 +0000

    Implement suggested changes
    In case of ambiguity, return first result.

--- getLocation.py
+++ getLocation.py
@@ -11,16 +11,11 @@ def getLocation(location):
    #send query, get json, ???, profit!
    response = urllib2.urlopen(url)
    data = json.load(response)
-   #print data
 
    #if google reports an error, we propagate it to our caller
    if data['status']!='OK':
       raise Exception("Unexpected status returned: " + str(data['status']))
 
-   #if we get several results, we can't determine what our caller wants, so we raise an exception.
-   if len(data['results'])>1:
-      raise Exception("Ambigous result returned: ") #removed printing this, was cluttering up output. Please change in future to use first result returned as opposed to raising exception
-   #we know that results contains exactly one entry (since status != OK if we don't get any results)
    #extract and return the given location to our caller.
    return data['results'][0]['geometry']['location']
 
